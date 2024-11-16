8547c9c86ba1a2b0adc811953b795380e421e102
kakao16 <117866285+kakao16@users.noreply.github.com>
2024-11-16 19:08:53 +0100

    zadanie 4

---
+++ Lab2_Zad4.py
@@ -0,0 +1,26 @@
+
+
+class Fibonacci:
+    current, next = 0, 1
+    index = 0
+    def __init__(self, stop):
+        self.stop = stop
+        
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        if self.index < self.stop:
+            self.index += 1    
+            fib = self.current
+            self.current, self.next = self.next, self.current + self.next
+            return fib
+        else:
+            raise StopIteration
+  
+        
+fibonacci = Fibonacci(10)
+fibIter = iter(fibonacci)
+
+for i in fibIter:
+    print(i)
